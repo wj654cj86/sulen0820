f08e8d2ceb0888b229c49f655bac9610e1db1ae8
WilliamTu <wj654cj86@gmail.com>
2022-05-10 09:15:07 +0800

    V1.0.4 新增抽卡指令

---
+++ pages/api/card.js
@@ -0,0 +1,21 @@
+var q = [3, 20, 100];
+var e = ['sulen010 ', 'sulen0Cake ', 'sulen0Food '];
+export default async function handler(req, res) {
+	let t = '';
+	let s = 0;
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 3; j++) {
+			if (Math.random() * 100 < q[j]) {
+				s += j;
+				if (s == 20)
+					t += e[1];
+				else
+					t += e[j];
+				break;
+			}
+		}
+	}
+	if (s > 18)
+		t += ' (保底)';
+	res.status(200).json(t);
+}
